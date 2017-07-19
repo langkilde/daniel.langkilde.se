ed71e556764ec35a1d876b06828eacda9691a364
langkilde <daniel.langkilde@gmail.com>
2017-07-19 12:33:42 +0200

    add missing whitespace

--- src/components/profilesList.tsx
+++ src/components/profilesList.tsx
@@ -14,8 +14,7 @@ class ProfilesList extends React.Component<IProfilesListPros, any> {
   private static renderProfile(profile) {
     return (
       <li key={profile.platform}
-          className="mobile-specific-list">{profile.platform} -
-        <a href={profile.link}>{profile.link_text}</a>
+          className="mobile-specific-list">{profile.platform} - <a href={profile.link}>{profile.link_text}</a>
       </li>
     );
   }
